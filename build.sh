a06dd193f5d98223ebc3d7f0c6a711aeef99dbcb
jrcleber <cleber.jr@live.com>
2023-01-08 14:52:59 -0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,31 +1,23 @@
 #!/bin/sh
 
-source "$(pwd)/.sh/progress.sh"
-
 if [ "$DOCKER_ENV" = "true" ];
 then
-  echo -en "\x1b[33mEnabling environment variables for Docker\n\r"
-  echo -en "\x1b[33mDOCKER_ENV=$DOCKER_ENV\n\r"
+  echo "Enabling environment variables for Docker"
+  echo "DOCKER_ENV=$DOCKER_ENV"
+  echo
 fi
-
-start_spinner "\x1b[36m > removing dist \x1b[0m"
+echo "> removing dist"
 rm -rf ./dist
-stop_spinner $?
-
 echo
-start_spinner "\x1b[36m > transpiling... \x1b[0m"
+echo "> transpiling..."
 tsc
 cp ./src/env.yml ./dist/src
-stop_spinner $?
 
 echo
-echo -en "\x1b[1m\x1b[34m > Successfully build \x1b[0m "
-echo
+echo "> Successfully build "
 
 echo
-start_spinner "\e[1;32m > Starting application... \x1b[0m"
-sleep 2
-stop_spinner $?
+echo "> Starting application..."
 echo
 node ./dist/src/main.js
 
