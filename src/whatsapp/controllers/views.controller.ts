3d71f12328ea6059ef1673fb7a30850f4eec2ba8
jrcleber <cleber.jr@live.com>
2023-01-26 21:18:12 -0300

    view controller fix

--- src/whatsapp/controllers/views.controller.ts
+++ src/whatsapp/controllers/views.controller.ts
@@ -1,32 +1,26 @@
 import { Request, Response } from 'express';
-import { readFileSync } from 'fs';
-import { join } from 'path';
 import { Auth, ConfigService } from '../../config/env.config';
-import { AUTH_DIR } from '../../config/path.config';
 import { BadRequestException } from '../../exceptions';
 import { InstanceDto } from '../dto/instance.dto';
+import { RepositoryBroker } from '../repository/repository.manager';
 import { HttpStatus } from '../routers/index.router';
-import { JwtPayload } from '../services/auth.service';
 import { WAMonitoringService } from '../services/monitor.service';
 
 export class ViewsController {
   constructor(
     private readonly waMonit: WAMonitoringService,
     private readonly configService: ConfigService,
+    private readonly repository: RepositoryBroker,
   ) {}
 
-  public qrcode(request: Request, response: Response) {
+  public async qrcode(request: Request, response: Response) {
     const param = request.params as unknown as InstanceDto;
     const instance = this.waMonit.waInstances[param.instanceName];
     if (instance.connectionStatus.state === 'open') {
       throw new BadRequestException('The instance is already connected');
     }
     const type = this.configService.get<Auth>('AUTHENTICATION').TYPE;
-    const data: JwtPayload = JSON.parse(
-      readFileSync(join(AUTH_DIR, type, param.instanceName + '.json'), {
-        encoding: 'utf-8',
-      }),
-    );
+    const data = await this.repository.auth.find(instance.instanceName);
 
     const hash = type === 'jwt' ? `Bearer ${data[type]}` : data[type];
 
