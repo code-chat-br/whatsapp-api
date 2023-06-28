c4bd291759762677b37b9b74783468390204f53f
Jean Kássio <jeankassiocheib@gmail.com>
2023-06-28 03:17:34 -0300

    Increase payload limit to 100Mb

--- src/main.ts
+++ src/main.ts
@@ -34,8 +34,8 @@ function bootstrap() {
       methods: [...configService.get<Cors>('CORS').METHODS],
       credentials: configService.get<Cors>('CORS').CREDENTIALS,
     }),
-    urlencoded({ extended: true, limit: '1000mb' }),
-    json({ limit: '1000mb' }),
+    urlencoded({ extended: true, limit: '100mb' }),
+    json({ limit: '100mb' }),
     compression(),
   );
 
