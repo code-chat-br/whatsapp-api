a0ee436a8d6559bd291d283d18437a961f78c252
Danilo de Souza <dannilosn@gmail>
2025-08-18 10:23:35 -0300

    Fix: Undo last commit

--- src/whatsapp/services/whatsapp.service.ts
+++ src/whatsapp/services/whatsapp.service.ts
@@ -2309,7 +2309,7 @@ export class WAStartupService {
     const messages = await this.repository.message.findMany({
       where,
       orderBy: {
-        id: 'desc',
+        messageTimestamp: 'desc',
       },
       skip: query.offset * (query?.page === 1 ? 0 : (query?.page as number) - 1),
       take: query.offset,
