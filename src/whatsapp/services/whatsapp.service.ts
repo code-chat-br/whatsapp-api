52aa0a5bacdd48aad21ceb70114d2fe79b94ca1b
Danilo de Souza <dannilosn@gmail>
2025-08-25 13:17:22 -0300

    Feat: Adds a centralized axios in whatsapp.service.ts with connection pooling

--- src/whatsapp/services/whatsapp.service.ts
+++ src/whatsapp/services/whatsapp.service.ts
@@ -81,7 +81,7 @@ import {
 import { Logger } from '../../config/logger.config';
 import { INSTANCE_DIR, ROOT_DIR } from '../../config/path.config';
 import { join, normalize } from 'path';
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError, AxiosInstance } from 'axios';
 import qrcode, { QRCodeToDataURLOptions } from 'qrcode';
 import qrcodeTerminal from 'qrcode-terminal';
 import { Boom } from '@hapi/boom';
@@ -151,6 +151,7 @@ import {
   unlinkSync,
   writeFileSync,
 } from 'fs';
+import http from 'http';
 
 type InstanceQrCode = {
   count: number;
@@ -175,6 +176,18 @@ export class WAStartupService {
       this.configService,
       this.providerFiles,
     );
+
+    const httpAgent = new http.Agent({
+      keepAlive: true,
+      maxSockets: 100,
+      maxFreeSockets: 10,
+      timeout: 60000
+    })
+
+    this.axiosInstance = axios.create({
+      httpAgent,
+      timeout: 10000
+    })
   }
 
   private readonly logger = new Logger(this.configService, WAStartupService.name);
@@ -186,6 +199,7 @@ export class WAStartupService {
   private readonly stateConnection: InstanceStateConnection = { state: 'close' };
   private readonly databaseOptions: Database =
     this.configService.get<Database>('DATABASE');
+  private readonly axiosInstance: AxiosInstance;
 
   private endSession = false;
   public client: WASocket;
@@ -278,7 +292,7 @@ export class WAStartupService {
     try {
       if (this.webhook?.enabled) {
         if (this.webhook?.events && this.webhook?.events[event]) {
-          await axios.post(
+          await this.axiosInstance.post(
             this.webhook.url,
             {
               event: eventDesc,
@@ -289,7 +303,7 @@ export class WAStartupService {
           );
         }
         if (!this.webhook?.events) {
-          await axios.post(
+          await this.axiosInstance.post(
             this.webhook.url,
             {
               event: eventDesc,
@@ -323,7 +337,7 @@ export class WAStartupService {
     try {
       const globalWebhook = this.configService.get<GlobalWebhook>('GLOBAL_WEBHOOK');
       if (globalWebhook?.ENABLED && isURL(globalWebhook.URL)) {
-        await axios.post(
+        await this.axiosInstance.post(
           globalWebhook.URL,
           {
             event: eventDesc,
@@ -1527,7 +1541,7 @@ export class WAStartupService {
       const isURL = /http(s?):\/\//.test(mediaMessage.media as string);
 
       if (isURL) {
-        const response = await axios.get(mediaMessage.media as string, {
+        const response = await this.axiosInstance.get(mediaMessage.media as string, {
           responseType: 'arraybuffer',
         });
 
@@ -1922,7 +1936,7 @@ export class WAStartupService {
         jpegThumbnail: await (async () => {
           if (data.linkMessage?.thumbnailUrl) {
             try {
-              const response = await axios.get(data.linkMessage.thumbnailUrl, {
+              const response = await this.axiosInstance.get(data.linkMessage.thumbnailUrl, {
                 responseType: 'arraybuffer',
               });
               return new Uint8Array(response.data);
@@ -2410,7 +2424,7 @@ export class WAStartupService {
     try {
       let pic: WAMediaUpload;
       if (isURL(picture.image)) {
-        pic = (await axios.get(picture.image, { responseType: 'arraybuffer' })).data;
+        pic = (await this.axiosInstance.get(picture.image, { responseType: 'arraybuffer' })).data;
       } else if (isBase64(picture.image)) {
         pic = Buffer.from(picture.image, 'base64');
       } else {
